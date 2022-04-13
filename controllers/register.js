14e6a83f8fd292c084de71390371d3068f267ab1
griffinbaker12 <griffin_baker12@icloud.com>
2022-04-13 11:30:17 -0500

    edits

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,10 @@ const handleRegister = (req, res, knex, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback);
     })
-    .catch(err => res.status(400).json('Unable to register'));
+    .catch(err => {
+      console.log(err);
+      res.status(400).json('Unable to register');
+    });
 };
 
 module.exports = {

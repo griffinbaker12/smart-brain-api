ed18518da250aae174de96e72bc51406d40a998e
griffinbaker12 <griffin_baker12@icloud.com>
2022-04-13 11:25:28 -0500

    edits

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const knex = require('knex')({
   client: 'pg',
   connection: {

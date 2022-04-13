144ad71f1ba124c4b1c7d3260b7d5a3973468e51
griffinbaker12 <griffin_baker12@icloud.com>
2022-04-13 11:14:00 -0500

    db updates

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const cors = require('cors');
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '',
-    database: 'smart-brain',
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
